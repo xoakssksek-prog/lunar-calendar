1b18d38daab081346b5f66532437aab2889aa2bc
xoakssksek <xoakssksek@gmail.com>
2026-05-18 10:05:59 +0700

    수정후

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1941698470" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2769230357" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
